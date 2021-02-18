10b844fcb8316cc5800b348db43659d202b06ebe
Collin Poczatek <collin.poczatek@gmail.com>
2021-02-18 10:41:17 -0800

    reorg, delete unused

--- iac-azure/modules/azurerm_container_registry/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "acr_login_server" {
-  value = var.create_container_registry ? "${azurerm_container_registry.acr[0].login_server}" : null
-}
-
-output "acr_id" {
-  value = var.create_container_registry ? "${azurerm_container_registry.acr[0].id}" : null
-}
